d13f62391cf532e083809aadb9cfd1e23e8ff065
eddiefiggie <eddiefiggie@gmail.com>
2018-05-01 09:59:46 -0700

    Included error handling and ReadMe updates.

--- MongoConnect.java
+++ MongoConnect.java
@@ -7,23 +7,32 @@
 
 import com.mongodb.client.MongoDatabase; 
 import com.mongodb.MongoClient; 
-import com.mongodb.MongoCredential;  
+import com.mongodb.MongoCredential; 
+import com.mongodb.MongoException; 
 
 public class MongoConnect { 
    
     public static void main( String args[] ) {  
-      
-       // Creating a Mongo client 
-       MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
+        try {  
+            // Creating a Mongo client 
+            MongoClient mongo = new MongoClient( "localhost" , 27017 ); 
    
-       // Creating Credentials 
-       MongoCredential credential; 
-       credential = MongoCredential.createCredential("sampleUser", "myDb", 
-          "password".toCharArray()); 
-       System.out.println("Connected to the database successfully");  
+            // Creating Credentials 
+            MongoCredential credential; 
+            credential = MongoCredential.createCredential("ExampleUser", "Database_01", 
+                 "PlaceHolder".toCharArray()); 
+            System.out.println();
+            System.out.println("Connected to the database successfully."); 
+            System.out.println(); 
       
-       // Accessing the database 
-       MongoDatabase database = mongo.getDatabase("myDb"); 
-       System.out.println("Credentials ::"+ credential);     
+            // Accessing the database 
+            MongoDatabase database = mongo.getDatabase("myDb"); 
+            System.out.println("Credentials:  " + credential);
+            System.out.println();
+        } 
+        catch (MongoException e) {
+            System.err.println(e);
+            System.exit(1);
+        }
     } 
 }
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # JDBC-Connect-MongoDB
 
+
+Dependencies:
+
+The JDBC Driver, as well as all required class files can be downloaded:
+
+https://mongodb.github.io/mongo-java-driver/
+
+* Be sure to use the UBER JAR file.
+
+
 Launch:
 
 java -cp .:mongo-java-driver-3.6.3.jar MongoConnect
